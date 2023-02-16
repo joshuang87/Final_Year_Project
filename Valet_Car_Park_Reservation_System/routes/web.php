235e1dbb8eddd0062b5ffa690e64a59462f1f8fb
joshuang87 <joshuang43@gmail.com>
2023-02-16 12:50:36 +0800

    vue.js was installed in project

--- Valet_Car_Park_Reservation_System/routes/web.php
+++ Valet_Car_Park_Reservation_System/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/testing', function () {
+    return view('prototype.testing');
 });
