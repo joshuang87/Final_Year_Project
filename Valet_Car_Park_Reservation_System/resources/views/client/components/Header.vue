95bdb0b26d9eb84f0c90565e6b1758eb52c6b367
Leo Chong <leochong0108@gmail.com>
2023-06-09 13:53:44 +0800

    footer_to_dark

--- Valet_Car_Park_Reservation_System/resources/views/client/components/Header.vue
+++ Valet_Car_Park_Reservation_System/resources/views/client/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="container-fluid">
     <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-      
+
       <RouterLink to="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none navbar-brand">
           <p>Home logo</p>
       </RouterLink>
