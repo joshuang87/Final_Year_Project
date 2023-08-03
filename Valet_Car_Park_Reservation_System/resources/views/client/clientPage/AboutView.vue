bca8244cc7ff05e38ad3442477b1c3b703ec5f35
D210178C <d210178c@sc.edu.my>
2023-08-03 18:18:11 +0800

    更改了 client aboutview.vue 的ui

--- Valet_Car_Park_Reservation_System/resources/views/client/clientPage/AboutView.vue
+++ Valet_Car_Park_Reservation_System/resources/views/client/clientPage/AboutView.vue
@@ -1,14 +1,32 @@
-
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <template>
   <el-carousel :interval="4000" type="card" height="200px">
     <el-carousel-item v-for="item in 6" :key="item">
       <h3 text="2xl" justify="center">{{ item }}</h3>
     </el-carousel-item>
   </el-carousel>
+
+  <h2>Valet Car Park Reservation System</h2>
+  <div class="images">
+    <img v-for="(image, index) in images" :key="index" :src="image" />
+  </div>  
+</div>
 </template>
+
+<script>
+export default {
+  data(){
+    return {
+      images:[
+        "../../../picture/car parking system.jpg",
+        "../../../picture/OIP.jpeg",
+        "../../../picture/Smart-Parking-System.jpg",
+      ]
+    }
+  }
+}
+</script>
 
 <style scoped>
 .el-carousel__item h3 {
@@ -28,5 +46,3 @@
 }
 </style>
 
-  </div>
-</template>
