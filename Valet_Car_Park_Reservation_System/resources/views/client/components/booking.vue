83e2b73549fc535d1a23bf35a9fd75c168e679fd
Leo Chong <leochong0108@gmail.com>
2023-08-03 15:00:46 +0800

    book update

--- Valet_Car_Park_Reservation_System/resources/views/client/components/booking.vue
+++ Valet_Car_Park_Reservation_System/resources/views/client/components/booking.vue
@@ -1,35 +1,24 @@
 <template>
+
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-6">
                 <br>
-                <div class="container">
-                    <div class="scrollable-table">
-                        <table class="table">
-
-                            <thead>
-                                <tr>
-                                    <th>Parking Space ID</th>
-                                    <th>Parking Lot ID</th>
-                                    <th>Open Time</th>
-                                    <th>Close Time</th>
-                                    <th>Status</th>
-                                </tr>
-                            </thead>
-
-                            <tbody>
-                                <tr v-for="item in items" :key="item.id">
-                                    <td>{{ item.parking_space_id }}</td>
-                                    <td>{{ item.parking_lot_id }}</td>
-                                    <td>{{ item.open_time }}</td>
-                                    <td>{{ item.close_time }}</td>
-                                    <td>{{ item.status }}</td>
-                                </tr>
-                            </tbody>
-
-                        </table>
+                    <div class="container">
+                        <div class="scrollable-table">
+                            <div class="row">
+                            <!-- Use nested v-for to create the grid layout -->
+                                <div v-for="row in itemsAs2DArray" :key="`row-${row[0].parking_lot_id}`" class="col-md-6">
+                                    <div v-for="item in row" :key="item.parking_space_id" class="mb-3">
+                                    <!-- Use a button to display parking space ID -->
+                                    <button class="btn btn-primary" @click="onSpaceButtonClick(item)">
+                                        {{ item.parking_space_id }}
+                                    </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                </div>
             </div>
 
             <div class="col-md-6 border-start">
@@ -38,51 +27,31 @@
                         <div class="col-lg-6">
                             <div class="card shadow">
                                 <div class="card-body">
-                                    <h2 class="mb-4" style="text-align: center;">Book</h2>
-                                    <form class="form-container" @submit.prevent="submitBooking">
-
-                                        <div class="form-group">
-                                            <label class="form-label" for="car_plate">Car Plate:</label>
-                                            <input class="form-control" type="text" id="car_plate" v-model="booking.car_plate" required>
-                                        </div>
-                                        <hr/>    
-                                        <div class="form-group">
-                                            <label class="form-label" for="phone_number">Phone Number:</label>
-                                            <input class="form-control" type="tel" id="phone_number" v-model="booking.phone_number" required>
-                                        </div>
-                                        <hr/>
-                                        <div class="form-group">
-                                            <label class="form-label" for="email">Email:</label>
-                                            <input class="form-control" type="email" id="email" v-model="booking.email">
-                                        </div>
-                                        <hr/>
-                                        <div class="form-group">
-                                            <label class="form-label" for="parking_space_id">Parking Space ID:</label>
-                                            <input class="form-control" type="text" id="parking_space_id" v-model="booking.parking_space_id" required>
-                                        </div>
-                                        <hr/>
-                                        <div class="form-group">
-                                            <label class="form-label" for="parking_lot_id">Parking Lot ID:</label>
-                                            <input class="form-control" type="text" id="parking_lot_id" v-model="booking.parking_lot_id" required>
-                                        </div>
-                                        <hr/>
-                                        <div class="form-group">
-                                            <label class="form-label" for="date">Date:</label>
-                                            <input class="form-control" type="date" id="date" v-model="booking.date" required>
-                                        </div>
-                                        <hr/>
-                                        <div class="form-group">
-                                            <label class="form-label" for="time">Time:</label>
-                                            <input class="form-control" type="time" id="time" v-model="booking.time" required>
-                                        </div>
-                                        <hr/>
-                                        <div class="form-group">
-                                            <label class="form-label" for="duration">Duration (hours):</label>
-                                            <input class="form-control" type="number" id="duration" v-model="booking.duration" required>
-                                        </div>
-                                        <hr/>
-                                        <button class="btn btn-primary" type="submit">Book Car Park</button>
-                                    </form>
+                                    <h2 class="mb-4" style="text-align: center;">Cart</h2>
+                                    <table class="table">
+                                        <thead>
+                                        <tr>
+                                            <th>Parking Space ID</th>
+                                            <th>Parking Lot ID</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr v-for="cartItem in cart" :key="cartItem.parking_space_id">
+                                            <td>{{ cartItem.parking_space_id }}</td>
+                                            <td>{{ cartItem.parking_lot_id }}</td>
+                                            <td>
+                                            <button class="btn btn-danger" @click="removeFromCart(cartItem)">
+                                                Delete
+                                            </button>
+                                            </td>
+                                        </tr>
+                                        <br>
+                                            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#bookingModal">
+                                                Confirm
+                                            </button>
+                                        </tbody>
+                                    </table>
                                 </div>
                             </div>
                         </div>
@@ -91,6 +60,77 @@
             </div>
         </div>
     </div><br>
+
+    <div class="modal fade" id="bookingModal" tabindex="-1" role="dialog" aria-labelledby="bookingModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
+            <div class="modal-content">
+
+                <div class="modal-header">
+                    <h5 class="modal-title" id="bookingtModalLabel">Payment</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+
+                <div class="modal-body">
+                    <div class="container">
+                        <div class="row">
+
+                            <div class="col-md-6">
+                                <div class="card-body">
+                                    <h2 class="mb-4" style="text-align: center;">Book</h2>
+                                    <form class="form-container" @submit.prevent="submitBooking">
+                                        <div class="form-group">
+                                            <label class="form-label" for="car_plate">Car Plate:</label>
+                                            <input class="form-control" type="text" id="car_plate">
+                                        </div>
+                                        <hr>
+                                        <div class="form-group">
+                                            <label class="form-label" for="phone_number">Phone Number:</label>
+                                            <input class="form-control" type="tel" id="phone_number">
+                                        </div>
+                                        <hr>
+                                        <div class="form-group">
+                                            <label class="form-label" for="email">Email:</label>
+                                            <input class="form-control" type="email" id="email">
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+
+                            <div class="col-md-6">
+                                <table class="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col">Parking Space ID</th>
+                                            <th scope="col">Parking Lot ID</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr v-for="cartItem in cart" :key="cartItem.parking_space_id">
+                                            <td>{{ cartItem.parking_space_id }}</td>
+                                            <td>{{ cartItem.parking_lot_id }}</td>
+                                        </tr>
+                                        <tr>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="modal-footer d-flex justify-content-center">
+                    <button type="button" class="btn btn-primary" data-bs-dismiss="modal">
+                        Pay with Debit/Credit Card (Stripe)
+                    </button>
+                    <button type="button" class="btn btn-primary">Pay with TNG</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
 </template>
 
 <script>
@@ -103,6 +143,7 @@ import PaymentForm from 'V/components/PaymentForm.vue';
         data() {
             return {
                 items: [],
+                cart: [],
                 booking: {
                     car_plate: '',
                     phone_number: '',
@@ -114,6 +155,26 @@ import PaymentForm from 'V/components/PaymentForm.vue';
                     duration: 0
                 },
             };
+        },
+
+        computed: {
+            // Convert the 'items' array to a 2D array for grid layout
+            itemsAs2DArray() {
+
+                const colsPerRow = 2; // Set the number of columns per row
+                const result = [];
+                let tempRow = [];
+
+                this.items.forEach((item, index) => {
+                    tempRow.push(item);
+                    if ((index + 1) % colsPerRow === 0 || index === this.items.length - 1) {
+                        result.push(tempRow);
+                        tempRow = [];
+                    }
+                });
+
+                return result;
+            },
         },
 
         components: {
@@ -134,6 +195,27 @@ import PaymentForm from 'V/components/PaymentForm.vue';
                         console.error(error);
                     });
             },
+
+            onSpaceButtonClick(item) {
+                // Handle the button click event, you can access the item's data here
+                this.booking.parking_space_id = item.parking_space_id;
+                this.booking.parking_lot_id = item.parking_lot_id;
+                this.cart.push({
+                    parking_space_id: item.parking_space_id,
+                    parking_lot_id: item.parking_lot_id,
+                });
+                // Set other relevant data from 'item' to 'this.booking' as needed
+                // You can then use this data to pre-fill the form or take other actions
+            },
+
+            removeFromCart(cartItem) {
+                // Remove the selected item from the cart
+                const index = this.cart.indexOf(cartItem);
+                if (index !== -1) {
+                    this.cart.splice(index, 1);
+                }
+            },
+
             async redirectToStripe() {
                 // Make an API request to your Laravel backend
                 const response = await axios.post('api/reserve/payment',this.booking);
@@ -147,6 +229,7 @@ import PaymentForm from 'V/components/PaymentForm.vue';
                     console.error(error);
                 }
             },
+
             async startCheckout() {
                 const stripe = await loadStripe('pk_test_51NGxNDGmA7QKNFMQAHGPm8IVsFAp2dqD7P5tvURFQCz3iDIAu0kWnOJgZ2mpVGSatoJxAKQRguqFqWC9FxYWOno800PHJXypYc');
 
@@ -168,6 +251,7 @@ import PaymentForm from 'V/components/PaymentForm.vue';
                     console.error('Error:', error);
                 }
             },
+
             submitBooking() {
                 this.redirectToStripe();
                 // this.startCheckout();
@@ -183,6 +267,7 @@ import PaymentForm from 'V/components/PaymentForm.vue';
                 //     });
 
             },
+
             storePayment() {
                 axios.post('/api/reserve/payment/store',[
                     'dadad',
