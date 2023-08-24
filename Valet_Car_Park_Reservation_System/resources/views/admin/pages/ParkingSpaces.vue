cc181b2f1c5a48ae72766a18051eaf4a6cda4111
joshuang87 <joshuang43@gmail.com>
2023-08-24 10:21:47 +0800

    moveToLaptop

--- Valet_Car_Park_Reservation_System/resources/views/admin/pages/ParkingSpaces.vue
+++ Valet_Car_Park_Reservation_System/resources/views/admin/pages/ParkingSpaces.vue
@@ -2,17 +2,39 @@
     <div>
         <div>
             <h1>Parking Spaces Data</h1>
+            <el-button @click="resetDateFilter">reset date filter</el-button>
+            <el-button @click="clearFilter">reset all filters</el-button>
         </div>
 
         <div>
-        {{  parkingSpacesDataList }}
+            <el-table :data="parkingSpaceDataList" border fit>
+                <el-table-column prop="parking_space_id" label="Parking Space Id" />
+                <el-table-column prop="parking_lot_id" label="Parking Lot Id" />
+                <el-table-column prop="open_time" label="Open Time" />
+                <el-table-column prop="close_time" label="Close Time" />
+                <el-table-column fixed="right" label="Operations">
+                    <template #default>
+                        <el-button text type="danger" icon="Delete" @click="">Delete</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+            <!-- <template>
+                <el-table :data="tableData" border style="width: 100%">
+                    <el-table-column prop="date" label="Date" width="180" />
+                    <el-table-column prop="name" label="Name" width="180" />
+                    <el-table-column prop="address" label="Address" />
+                </el-table>
+            </template> -->
         </div>
     </div>
-
 </template>
 
-<script>
+<script setup>
     import axios from 'axios'
+    import { ref } from 'vue'
+
+    const parkingSpaceDataList = ref([])
 
     const getAllParkingSpaces = async()=>{
         try {
@@ -26,13 +48,9 @@
         }
     }
 
-    export default {
-        async setup() {
-            const parkingSpacesDataList = await getAllParkingSpaces()
+    const data = await getAllParkingSpaces()
+    parkingSpaceDataList.value = data
 
-            return {
-                parkingSpacesDataList
-            }
-        }
-    }
+    console.log(parkingSpaceDataList.value)
+
 </script>
